654927a8536301ededa0fcc55019c80460d26b6a
rakib119 <rakibh01739@gmail.com>
2022-10-30 16:47:14 +0600

    initiate project

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,6 +16,6 @@ class UserSeeder extends Seeder
 
     public function run()
     {
-        
+        User::factory(1000)->create();
     }
 }
